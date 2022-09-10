7abc500d981fb5ee8390b7e84d135c6b32455cff
OluwabukolaU <oluwabukola.umaru@gmail.com>
2022-09-10 07:11:24 -0700

    TASK 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,20 @@
-#include <stdio.h>                                                                                                                                                      
-/**                                                                                                                                                                     
-*main-program entry point.                                                                                                                                              
-*Return:0 no error, non zero if error                                                                                                                                   
-*/                                                                                                                                                                      
-int main(void)                                                                                                                                                          
-{                                                                                                                                                                       
-        int i;                                                                                                                                                          
-                                                                                                                                                                        
-        for (i = '0'; i <= '9'; i++)                                                                                                                                    
-        {                                                                                                                                                               
-                putchar(i);                                                                                                                                             
-        }                                                                                                                                                               
-                putchar('\n');                                                                                                                                          
-                                                                                                                                                                        
-        return (0);                                                                                                                                                     
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
 }
