c3693e20308f984cc7bc2eba5b2ce217832aa258
OluwabukolaU <oluwabukola.umaru@gmail.com>
2022-09-13 13:36:46 -0700

    TASK 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @b1: integer 1
+ * @b2: integer 2
+ * Description: give two integers, returns sum
+ * Return: sum
+ */
+
+int add(int b1, int b2)
+{
+	int sum;
+
+	sum = b1 + b2;
+
+	return (sum);
+}
+
+
+
+
+
+
