851687bada4bda7df1268feb9ba96857e1445513
OluwabukolaU <oluwabukola.umaru@gmail.com>
2022-09-13 12:50:55 -0700

    TASK 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,9 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: n is an integer
- * Return: 1 (if positive)
- * Return:  0 (if zero),
- * Return:  -1 (if negative)
+ * Return: 1 (if positive), 0 (if zero),  -1 (if negative)
  */
 
 int print_sign(int n)
