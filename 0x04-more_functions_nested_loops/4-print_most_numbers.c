a513dbe34d86ed5acbbe4c13812ed7a2f28ba45c
OluwabukolaU <oluwabukola.umaru@gmail.com>
2022-09-15 16:07:26 -0700

    Prints numbers 0 to 9 except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,4 @@
- #include "main.h"
+#include "main.h"
 
 /**
  * print_most_numbers - prints from 0 to 9 except 2 and 4
@@ -18,5 +18,3 @@ void print_most_numbers(void)
 	}
 	_putchar('\n');
 }
-Footer
-
