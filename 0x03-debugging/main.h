3dd8c93f97feda373c5308e8294276e007c61f09
OluwabukolaU <oluwabukola.umaru@gmail.com>
2022-09-15 13:27:30 -0700

    est that the function positive_or_negative

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
 
-#include <stdio.h>
-
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
-
-#endif /* MAIN_H */
-
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
