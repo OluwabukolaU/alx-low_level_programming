c9c6ef2d4946ae90e063be90cfba147d6e95661f
OluwabukolaU <oluwabukola.umaru@gmail.com>
2022-09-15 13:15:48 -0700

    TASK O

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,12 @@
 /**
- * main - tests function that prints if integer is positive or negative
+ * main - prints positive or negative integer
  * Return: 0
  */
-
-int main (void)
+int main(void)
 {
 	int i;
-
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
+
